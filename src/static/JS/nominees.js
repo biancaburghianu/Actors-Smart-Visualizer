a83bd211dc96fb4c07266e8299c4e100e2462a60
chirilaciprian <79197823+chirilaciprian@users.noreply.github.com>
2023-04-10 02:45:41 +0300

    nominees

--- src/static/JS/nominees.js
+++ src/static/JS/nominees.js
@@ -1,4 +1,11 @@
 const CardsContainer = document.querySelectorAll(".CardsContainer");
+const overlay = document.querySelectorAll(".overlayCard");
+const ActorsOverlay = document.querySelector(".overlayCard.Actors");
+const CastOverlay = document.querySelector(".overlayCard.Cast");
+const closebtn = document.querySelectorAll(".overlaybtn");
+const overlaybtn = document.querySelectorAll(".CardBtn");
+const ActorsCardBtn = document.querySelectorAll(".ActorsCardBtn");
+const Actorsoverlaybtn = document.querySelectorAll(".ActorsCardBtn");
 CardsContainer.forEach((Container) => {
   let Cards = Container.querySelectorAll(".Card");
   Cards.forEach((Card) => {
@@ -14,3 +21,22 @@ function removeActive(CardsVariable) {
     Card.classList.remove("active");
   });
 }
+closebtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // overlay.classList.remove("active");
+    overlay.forEach((ovr) => {
+      ovr.classList.remove("active");
+    });
+  });
+});
+
+overlaybtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    CastOverlay.classList.add("active");
+  });
+});
+ActorsCardBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    ActorsOverlay.classList.add("active");
+  });
+});
