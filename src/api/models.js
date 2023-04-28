595f2a2c0c073c0bb3c8556633505ef258620f72
Burghianu Bianca <biancaburghianu2001@gmail.com>
2023-04-28 12:01:32 +0300

    created user model with sequelize

---
+++ src/api/models.js
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "./sequelizeConfig.js";
+class User extends Model {}
+
+User.init(
+  {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
+
+User.sync(); //creeaza tabelul daca nu exista
+
+console.log(User === sequelize.models.User);
