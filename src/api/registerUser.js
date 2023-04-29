e58d05f0b3c78e53ef8e5734784f31d81f4035e5
Burghianu Bianca <biancaburghianu2001@gmail.com>
2023-04-29 12:44:44 +0300

    adding new users to db by hashing password

---
+++ src/api/registerUser.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import { User } from "./models.js";
+
+export async function registerUser(username, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username: username,
+    password: hashedPassword,
+  });
+
+  return user;
+}
