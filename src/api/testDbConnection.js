d403082fb5d3fa3afe30ce5d7d8a08625c4ca7df
Burghianu Bianca <biancaburghianu2001@gmail.com>
2023-04-29 12:41:49 +0300

    function to test the db Connection with sequelize

---
+++ src/api/testDbConnection.js
@@ -0,0 +1,10 @@
+import { sequelize } from "./sequelizeConfig.js";
+
+export async function testDbConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
